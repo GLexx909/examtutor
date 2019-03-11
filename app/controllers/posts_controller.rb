ba1613d8348eeb9884095293fbc24f0ee706c02f
Vitaly Simonov <guardian909@rambler.ru>
2019-03-11 12:42:53 +0300

    Create Post.tutor_index view

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,23 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!
+
   authorize_resource
 
   def tutor_index
     @posts = Post.all
+  end
+
+  def create
+    post
+  end
+
+  private
+
+  def post
+    @post ||= params[:id] ? Post.find(params[:id]) : Post.new(post_params)
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :body)
   end
 end
