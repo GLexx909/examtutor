7df13b05a638eaaa60dfbeba36500812bfd5ffcb
Vitaly Simonov <guardian909@rambler.ru>
2019-03-23 15:31:06 +0300

    Generate Models Topic, TopicPassage

---
+++ db/migrate/20190323122813_create_topic_passages.rb
@@ -0,0 +1,11 @@
+class CreateTopicPassages < ActiveRecord::Migration[5.2]
+  def change
+    create_table :topic_passages do |t|
+      t.references :user, foreign_key: true
+      t.references :modul, foreign_key: true
+      t.boolean :status, default: false
+
+      t.timestamps
+    end
+  end
+end
---
+++ spec/factories/topics.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :topic do
+    title { "MyString" }
+    body { "MyString" }
+    modul { nil }
+  end
+end
