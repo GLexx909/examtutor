dd9af405692f8c99c3f79820941d4298ec5b2e76
Vitaly Simonov <guardian909@rambler.ru>
2019-03-24 16:09:33 +0300

    Answer. Add dependences

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_24_121119) do
+ActiveRecord::Schema.define(version: 2019_03_24_130234) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "answers", force: :cascade do |t|
+    t.string "body", null: false
+    t.boolean "correct", default: false, null: false
+    t.bigint "question_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["question_id"], name: "index_answers_on_question_id"
+  end
 
   create_table "course_passages", force: :cascade do |t|
     t.bigint "user_id"
@@ -148,6 +157,7 @@ ActiveRecord::Schema.define(version: 2019_03_24_121119) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "answers", "questions"
   add_foreign_key "essay_passages", "essays"
   add_foreign_key "essay_passages", "users"
   add_foreign_key "modul_passages", "moduls"
