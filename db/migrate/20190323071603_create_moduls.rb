893ec3571007848d5311b3e844c7bb6f8448b3d0
Vitaly Simonov <guardian909@rambler.ru>
2019-04-02 13:39:14 +0300

    Build. Modul model add columns index, status

--- db/migrate/20190323071603_create_moduls.rb
+++ db/migrate/20190323071603_create_moduls.rb
@@ -1,8 +1,11 @@
 class CreateModuls < ActiveRecord::Migration[5.2]
   def change
     create_table :moduls do |t|
+      t.integer :index
       t.string :title
+      t.boolean :status, default: false
       t.belongs_to :course
+
       t.timestamps
     end
   end
